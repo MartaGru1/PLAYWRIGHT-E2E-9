d3b8a4b7026678be0d9ba3650712c8992385b56a
MartaGru1 <marta.hrusht0211@gmail.com>
2024-07-22 21:25:10 -0500

    new info

--- .github/workflows/helpers/clickHelpers.ts
+++ .github/workflows/helpers/clickHelpers.ts
@@ -2,22 +2,22 @@ import { Page } from "@playwright/test";
 
 /**
  * Method to click on the button depending on its visible text.
- *
- * @param page
- * @param buttonText
+ * 
+ * @param page 
+ * @param buttonText 
  */
 async function clickButton(page: Page, buttonText: string) {
-  await page.getByRole("button", { name: buttonText }).click();
+  await page.getByRole('button', { name: buttonText }).click()
 }
 
 /**
  * Method to click on the link depending on its visible text.
- *
- * @param page
- * @param buttonText
+ * 
+ * @param page 
+ * @param buttonText 
  */
 async function clickLink(page: Page, linkText: string) {
-  await page.getByRole("link", { name: linkText }).click();
+  await page.getByRole('link', { name: linkText }).click()
 }
 
-export { clickButton, clickLink };
+export { clickButton, clickLink }
--- tests/integration/02-locators.spec.ts
+++ tests/integration/02-locators.spec.ts
@@ -1,4 +1,4 @@
-import { test, Locator } from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
 
 // test.use({
 //   launchOptions: {
@@ -61,5 +61,76 @@ test.describe("Playwright Locators", () => {
     const unorderedList = page.locator('#unordered_list')
 
     const childItem = unorderedList.locator('#unordered_list_item1')
+  })
+
+  test('Playwright - Handling multiple elements', async({ page }) => {
+
+    await page.goto("https://techglobal-training.com/frontend");
+    await page.locator('a:has-text("HTML Elements")').click()
+
+    const unorderedList = page.locator('#unordered_list > li')
+
+    await unorderedList.first().click()
+    await unorderedList.nth(1).click()
+    await unorderedList.last().click()
+
+    console.log('SPECIFIC TEST')
+
+    // This will return a failure because Playwright will find more than 1 element which violates the 'strict' mode
+    // await unorderedList.click()
+
+    const checkboxGroup = page.locator('#checkbox-button-group input')
+
+    const checkboxCount = await checkboxGroup.count()
+
+    for(let i = 0; i < checkboxCount; i++) {
+      await checkboxGroup.nth(i).check()
+    }
+
+    const checkboxCount2 = await page.locator('#checkbox-button-group input').all()
+
+    for(const checkbox of checkboxCount2 ) {
+      await checkbox.check()
+      // await expect(checkbox).toBeVisible()
+    }
+  })
+
+  test('Playwright - Built-in locators', async({ page }) => {
+    await page.goto("https://techglobal-training.com/frontend");
+
+    await page.getByRole('link', { name: 'HTML Elements'}).click()
+
+    await page.getByRole('heading', { name: 'HTML Elements'}).highlight()
+
+    await page.getByRole('button', { name: 'Register'}).click()
+
+    await page.getByPlaceholder('Enter text here').fill('TechGlobal')
+  })
+
+
+  test('Playwright - filter() locator API', async({ page }) => {
+    await page.goto('https://www.techglobal-training.com/frontend')
+
+    // await page.locator('a:has-text("Html elements")').click();
+    await page.getByRole('link', { name: 'Html Elements' }).click()
+
+    const testingParagraps = page.locator('p').filter({ hasText: 'testing'})
+    await testingParagraps.highlight()
+
+    const languageHeadings = await page.locator('label').count()
+
+    console.log(`Amount of elements with label tag is: ${languageHeadings}`)
+
+    const noneLanguageHeadings = await page.locator('label').filter({ hasNotText: 'Java' }).count()
+
+    console.log(`Amount of elements with label tag is but Java: ${noneLanguageHeadings}`)
+
+    const wrappers = await page.locator('[data-identifier*="a"]').count()
+
+    console.log(`Locaterd wrappers are: ${wrappers}`)
+
+    const uniqueWrapper = await page.locator('[data-identifier*="a"]').filter({ has: page.locator('#java_radio') }).count()
+
+    console.log(`Locaterd wrapper is: ${uniqueWrapper}`)
   })
 });
